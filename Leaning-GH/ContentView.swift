05ac4bdb02cd789d2dc28638ff89b9812758f1e3
Ajaaypranav Ramaiya Krisshnan <ajaay@zetran.com>
2022-04-24 07:24:20 +0530

    Initial Commit

---
+++ Leaning-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leaning-GH
+//
+//  Created by Ajaaypranav R K on 24/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Leaning-GH/Leaning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leaning_GHApp.swift
+//  Leaning-GH
+//
+//  Created by Ajaaypranav R K on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Leaning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
